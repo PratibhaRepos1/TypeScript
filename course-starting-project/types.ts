c85c954e099e9f095e595e34173c934c74331484
PratibhaRepos1 <pratibha.jadhav3229@gmail.com>
2022-01-03 11:01:24 +0530

    annotations in typescript

--- course-starting-project/types.ts
+++ course-starting-project/types.ts
@@ -37,5 +37,17 @@ let speed: string = 'fast';
      // z:30 //annotations error is here
   };
 
+ //function annotations
+ const logNumber = (i) => {
+    console.log(i);
+ }
 
+ //variable whose type can not be inferred correctly
+ let numbers = [-1,-12, 12];
+ let numbersAboveZero: boolean | number = false;
+ for(let i = 0; i < numbers.length; i++){
+     if(numbers[i] > 0)
+     numbersAboveZero = numbers[i]; //Type 'number' is not assignable to type 'boolean'. so use one more type here to avoid this error
+     console.log(numbersAboveZero);
+ }
 
