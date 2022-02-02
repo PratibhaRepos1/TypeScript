96f3da82bd67cc49db1db67370bd71023e9f2478
PratibhaRepos1 <pratibha.jadhav3229@gmail.com>
2022-02-02 21:47:19 +0530

    latest code

--- course-starting-project/maps/src/index.ts
+++ course-starting-project/maps/src/index.ts
@@ -1,8 +1,23 @@
 import { User, red } from "./User";
 import { Company } from "./Company";
+// const user = new User();
+// console.log(user);
+// const company = new Company();
+// console.log(company);
 
+interface Mappable {
+    location:{
+        lat: number;
+        lng:number;
+    }
+}
+import { customMap } from "./customMaps";
 const user = new User();
-console.log(user);
-
 const company = new Company();
-console.log(company);
+
+const customMaps = new customMap('map');
+
+customMaps.addMarker(user);
+customMaps.addMarker(company);
+
+
